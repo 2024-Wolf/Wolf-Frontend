8abc9180e3a669613a90598dea8f286d2ebb64ce
myeongju00 <j51677767@gmail.com>
2024-09-19 18:31:12 +0900

    feat : 메인페이지 카드 컴포넌트 제작

---
+++ src/components/Card/Category.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+
+const CategoryContainer = styled.div`
+    width: 100%;
+    height: 110px;
+    position: relative;
+    background-image: ${(category) =>
+            category === "스터디"
+                    ? `url('/ProjectThumbnail.png')`
+                    : `url('/StudyThumbnail.png')`};
+    background-size: cover;
+    background-position: center;
+    border-radius: 30px 30px 0 0;
+    
+    /* 반응형 미디어 쿼리 */
+    @media (max-width: 768px) {
+        height: 90px; /* 태블릿 크기 이하일 때 높이 조정 */
+    }
+
+    @media (max-width: 480px) {
+        height: 70px; /* 모바일 크기 이하일 때 높이 조정 */
+    }
+`;
+
+const CategoryTag = styled.span`
+    background-color: var(--violet500); /*color system 500*/
+    color: white;
+    border-radius: 10px;
+    padding: 4px 8px;
+    font-size: 12px;
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    z-index: 1;
+`;
+
+
+const Category = ({ category }) => {
+    return (
+        <CategoryContainer>
+            <CategoryTag>#{category}</CategoryTag>
+        </CategoryContainer>
+    );
+}
+
+export default Category;
