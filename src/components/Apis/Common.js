aa279a9a6c84664fc5a9a652203971a7ea6a8667
1soo <aaa4799@naver.com>
2024-10-16 11:37:17 +0900

    작업 중

--- src/components/Apis/Common.js
+++ src/components/Apis/Common.js
@@ -1,5 +1,5 @@
 export const BASE_URL = "http://localhost:8080/api/v1";
-export let accessToken = "Bearer eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiIxIiwiVXNlclJvbGVUeXBlIjoiVVNFUiIsImlhdCI6MTcyODk1MDQzNywiZXhwIjoxNzI4OTU0MDM3fQ.Kfv5InB5GBGElrxCUMO7G0p-kAUsVQZRQzroc8AXnq_xG2t685azy5-_vt-_fy3c";
+export let accessToken = "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiIxIiwiVXNlclJvbGVUeXBlIjoiVVNFUiIsImlhdCI6MTcyODk4MzExNywiZXhwIjoxNzI4OTg2NzE3fQ.Y6POMeXkf1Rdni-fcyFz4kHQqMVP-12CGlYoZd76abNQCg_9yBfLD20dCuevUI00";
 export let refreshToken = "Bearer eyJhbGciOiJIUzM4NCJ9.eyJleHAiOjE3Mjk1NTUyMzd9.1AEDIEFB17TtGliNB7Zf7LaxZpf9brJHuk3xTM2psfxlac0Ow9s1dfOHCsxyE8Bi";
 
 export function setAccessToken(token){
