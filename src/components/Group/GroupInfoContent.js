fa5a1fcd7cf5fb23ab86fc1da471c79c4a5480fa
이정연 <ejung6230@naver.com>
2024-10-25 05:04:27 +0900

    fix: 평가하기 모달 열기 닫기 추가

--- src/components/Group/GroupInfoContent.js
+++ src/components/Group/GroupInfoContent.js
@@ -23,7 +23,7 @@ import ImagePreview from "../Img/ImagePreview"
 const GroupInfoContent = ({ mode, groupPostId, userId, groupPostData, groupNewsData }) => {
 
   const [isModalOpen, setModalOpen] = useState(false);
-
+  const [isEvaluationModalOpen, setIsEvaluationModalOpen] = useState(false);
 
   const openModal = () => {
     setModalOpen(true);
@@ -33,16 +33,10 @@ const GroupInfoContent = ({ mode, groupPostId, userId, groupPostData, groupNewsD
     setModalOpen(false);
   };
 
+
+
   return (
     <>
-      {/* 평가하기 모달 테스트중 */}
-      <EvaluationModal
-        userId={userId}
-        onClose={closeModal}
-        isView={false}
-        optionalRequirements={groupPostData?.optionalRequirements}
-        groupId={groupPostId} />
-
       <InfoContainer>
         {/* 이미지가 있으면 여기에 넣으면 됨! */}
         <ImagePreview
@@ -139,6 +133,18 @@ const GroupInfoContent = ({ mode, groupPostId, userId, groupPostData, groupNewsD
           />
         </Violet500LineDiv>
       </InfoContainer >
+      {/* 평가하기 모달 테스트 */}
+      <button type="button" onClick={() => setIsEvaluationModalOpen(true)}>
+        평가하기 모달 열기
+      </button>
+      {
+        isEvaluationModalOpen && <EvaluationModal
+          userId={userId}
+          onClose={() => setIsEvaluationModalOpen(false)}
+          isView={false}
+          optionalRequirements={groupPostData?.optionalRequirements}
+          groupId={groupPostId} />
+      }
     </>
   );
 };
